77290077c033220da032c89eaae142d3b48c15aa
T.Saito <saito@koyo-se.net>
2017-01-31 19:17:14 +0800

    commit

--- samples/btc/btcChart.js
+++ samples/btc/btcChart.js
@@ -44,27 +44,17 @@ var config = {
 };
 
 var btcDatasets = {
-  // format:true,
   volume: {
     data: volume,
-    // color: 'rgb(50,50,80)',
-    // fill: 0.5,
-    // colorYaxis: 'rgb(150,150,180)',
   },
   lasts: {
     data: lsb[0],
-    // color: 'rgb(255, 205, 86)',
-    // colorYaxis: 'rgb(255,205,86)',
   },
   sells: {
     data: lsb[1],
-    // color: 'rgb(255,90,90)',
-    // fill: 0.3,
   },
   buys: {
     data: lsb[2],
-    // color: 'rgb(90,255,90)',
-    // fill: 0.3,
   },
 };
 
